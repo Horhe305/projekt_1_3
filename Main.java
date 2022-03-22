9d17c42bcf31e79ee5a1bb49d655b61a00df40b8
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-03-22 11:57:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int x = 5;
+    System.out.println(x);
+    float y = 2;
+    System.out.println(y);
+    double z = 3;
+    System.out.println(z);
+    String f = "napis";
+    System.out.println(f);
+  }
+}
